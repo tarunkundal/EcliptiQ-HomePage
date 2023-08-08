9e52198eea5768bc5402485ade2bed4e3f4f1334
Tarun Chauhan <chauhan271singh@gmail.com>
2023-08-08 18:07:03 +0530

    modified comp2

--- src/screens/Home/Comp2.tsx
+++ src/screens/Home/Comp2.tsx
@@ -8,54 +8,66 @@ const Comp2 = () => {
 			<Text align="center" fontWeight="bold" fontSize="14px">
 				JOIN 800,000+ HIGHLY PRODUCTIVE TEAMS
 			</Text>
-			<Flex justifyContent="space-around">
+			<Flex
+				justifyContent="space-around"
+				display={{ base: 'inline-grid', md: 'flex' }}
+				alignItems="center"
+			>
 				<ChakraImage
-					width={{ base: 40, md: 90 }}
+					width={{ base: 30, md: 90 }}
 					height={{ base: 5, md: 70 }}
+					m={2}
 					src="/booking_com.svg"
 					alt="booking.com"
 				/>
 				<ChakraImage
-					width={{ base: 40, md: 90 }}
+					width={{ base: 30, md: 90 }}
 					height={{ base: 5, md: 70 }}
 					src="/ibm.svg"
 					alt="ibm"
+					m={2}
 				/>
 				<ChakraImage
-					width={{ base: 40, md: 90 }}
+					width={{ base: 30, md: 90 }}
 					height={{ base: 5, md: 70 }}
 					src="/logitech.svg"
 					alt="logitech"
+					m={2}
 				/>
 				<ChakraImage
-					width={{ base: 40, md: 90 }}
+					width={{ base: 30, md: 90 }}
 					height={{ base: 5, md: 70 }}
 					src="/fortinet.svg"
 					alt="fortinet"
+					m={2}
 				/>
 				<ChakraImage
-					width={{ base: 40, md: 90 }}
+					width={{ base: 30, md: 90 }}
 					height={{ base: 5, md: 70 }}
 					src="/tibco.svg"
 					alt="tibco"
+					m={2}
 				/>
 				<ChakraImage
-					width={{ base: 40, md: 90 }}
+					width={{ base: 30, md: 90 }}
 					height={{ base: 5, md: 70 }}
 					src="/spotify.svg"
 					alt="spotify"
+					m={2}
 				/>
 				<ChakraImage
-					width={{ base: 40, md: 90 }}
+					width={{ base: 30, md: 90 }}
 					height={{ base: 5, md: 70 }}
 					src="/t-mobile.svg"
 					alt="t-mobile"
+					m={2}
 				/>
 				<ChakraImage
-					width={{ base: 40, md: 90 }}
+					width={{ base: 30, md: 90 }}
 					height={{ base: 5, md: 70 }}
 					src="/netflix.svg"
 					alt="netflix"
+					m={2}
 				/>
 			</Flex>
 		</Stack>
